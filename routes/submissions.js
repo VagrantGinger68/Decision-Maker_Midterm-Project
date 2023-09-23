f5fbfe63f37796c957dde66f90b6fe883c2b2f58
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-09-23 14:18:52 -0400

    added routes for submissions

---
+++ routes/submissions.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const pollQueries = require('../db/polls');
+
+router.get("/:id", (req, res) => {
+  const pollId = req.params.id;
+
+  // using database
+  // pollQueries.getPollById(pollId)
+  //   .then((poll) => {
+  //     const templateVars = poll;
+  //     return res.render("submission", templateVars);
+  //   });
+
+  // temporary data for testing
+  const templateVars = {
+    title: "Poll Title",
+    question: "Question",
+    choices: ["option 1", "option 2", "option 3", "option 4", "option 5"]
+  };
+  return res.render("submission", templateVars);
+});
+
+router.post("/:id", (req, res) => {
+  const pollId = req.params.id;
+
+  pollQueries.insertPoll(pollId)
+    .then((poll) => {
+      const templateVars = poll;
+      return res.render("submissionComplete", templateVars); // temporary page name until decision on what happens upon submission is made
+    });
+});
+
+module.exports = router;
