208c7156b73fb872f913ee41e66057480c0136e9
ritatanght <doggymint@gmail.com>
2023-09-22 22:58:33 -0400

    Add sample data for choices

---
+++ db/seeds/03_choices.sql
@@ -0,0 +1,17 @@
+-- description column is optional and default as ''
+
+INSERT INTO choices (title, poll_id)
+VALUES
+('Pepperoni','5Wq2sD'),
+('Pineapple','5Wq2sD'),
+('Just cheese','5Wq2sD'),
+('Mushrooms','5Wq2sD');
+
+
+INSERT INTO choices (title, poll_id)
+VALUES
+('Birds','q35Wha'),
+('Cats','q35Wha'),
+('Dogs','q35Wha'),
+('Fish','q35Wha'),
+('Hamsters','q35Wha');
