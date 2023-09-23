28d21557857b35fece4931729fbcdd00f7d90b4a
Rita Tang <doggymint@gmail.com>
2023-09-22 21:45:30 -0400

    Add schema for choices

---
+++ db/schema/03_choices.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS choices CASCADE;
+
+CREATE TABLE choices (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT DEFAULT '',
+  choice_id INTEGER REFERENCES choices(id) ON DELETE CASCADE
+);
