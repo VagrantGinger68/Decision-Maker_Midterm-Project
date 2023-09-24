c59f21ba2951f7214c5212a15f1a24fa29ae4a69
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-09-24 15:06:55 -0400

    Added slide to the submit form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,11 @@
 // Client facing scripts here
+$(document).ready(function() {
+
+  $("#create-poll").hide();
+
+  $("#submit-poll-btn").click(function() {
+    $("#create-poll").slideToggle(500, function() {
+      $("#email").focus();
+    });
+  });
+});
