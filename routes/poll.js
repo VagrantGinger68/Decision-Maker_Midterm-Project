f84dba2b6a4a8d0d75797582185e487c5eab4af4
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-09-24 13:51:28 -0400

    Updated email layout

--- routes/poll.js
+++ routes/poll.js
@@ -18,9 +18,11 @@ router.post("/", (req, res) => {
     from: "Decision Maker <postmaster@sandboxc01919f6d65b40f7bba14ed0dc9d97bd.mailgun.org>",
     to: email,
     subject: "Here's your Poll!",
-    html: `<h1 style='display:flex; align-items:center;'><img style='height:100px;' src='http://localhost:8080/images/Decision-Maker-Logo.png'> Decision Maker</h1>
-    <h3>Poll Results: "random url here"</h3>
-    <h3>Share Poll: "random url here"</h3>
+    html: `<div style='height: 100vh; background-color: #fcf4de; display: flex; flex-direction: column; align-items:center;'><img style='height: 400px' src='http://localhost:8080/images/Decision-Maker-Logo.png'>
+    <h1>Here are the links for your poll!</h1>
+    <h2>Poll Results: "random url here"</h2>
+    <h2>Share Poll: "random url here"</h2>
+    </div>
     `
   };
   mailgun().messages().send(emailInfo);
