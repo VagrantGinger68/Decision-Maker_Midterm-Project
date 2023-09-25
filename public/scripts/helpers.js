937e7847c7fc1ac07710c8fa4777afd4bd86a20a
Rita Tang <doggymint@gmail.com>
2023-09-25 18:53:58 -0400

    Add generateResults and rankChoices helper functions

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,8 +1,9 @@
-const pollQueries = require('../../db/queries/polls');
+const pollQueries = require("../../db/queries/polls");
 
 //Generate a random 6 digit string
-const generateRandomString = function() {
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const generateRandomString = function () {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let randomString = "";
 
   for (let i = 0; i < 6; i++) {
@@ -13,11 +14,11 @@ const generateRandomString = function() {
   return randomString;
 };
 
-const getChoices = function(body) {
+const getChoices = function (body) {
   const choices = [];
 
   for (let key in body) {
-    if (key.startsWith('option')) {
+    if (key.startsWith("option")) {
       choices.push(body[key]);
     }
   }
@@ -25,10 +26,45 @@ const getChoices = function(body) {
   return choices;
 };
 
-const insertAllChoices = function(choices, pollId) {
+const insertAllChoices = function (choices, pollId) {
   for (let choice of choices) {
     pollQueries.insertChoices([choice, pollId]);
   }
 };
 
-module.exports = { generateRandomString, getChoices, insertAllChoices };
+// takes in a nested array of arrays and sum all the points for all the choices into a single array
+const generateResults = function (results) {
+  const totalPoints = results[0];
+  for (let i = 1; i < results.length; i++) {
+    for (let j = 0; j < results[i].length; j++) {
+      totalPoints[j] += results[i][j];
+    }
+  }
+  return totalPoints;
+};
+
+// rank choices by the points for each of the choices
+const rankChoices = function(choices, results) {
+  const indices = [];
+  for (const ind of results.keys()) {
+    indices.push(ind);
+  }
+
+  // Sort the indices based on the corresponding results values
+  indices.sort((a, b) => results[b] - results[a]);
+
+  // Create the rankedChoices array based on the sorted indices
+  const rankedChoices = [];
+  for (let i = 0; i < indices.length; i++) {
+    rankedChoices.push(choices[indices[i]]);
+  }
+  return rankedChoices;
+};
+
+module.exports = {
+  generateRandomString,
+  getChoices,
+  insertAllChoices,
+  generateResults,
+  rankChoices,
+};
