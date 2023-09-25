b4d406395302b929499005adacbd464c803bb5ec
Rita Tang <doggymint@gmail.com>
2023-09-25 18:54:53 -0400

    Refractor to get results from database

--- routes/results.js
+++ routes/results.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const { getResultsById } = require("../db/queries/results");
+const { getPollById, getChoicesByPoll } = require("../db/queries/polls");
+const { generateResults, rankChoices } = require("../public/scripts/helpers");
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
-  // get results from database
 
-  // sample data for html display
-  const templateVars = {
-    title: "Poll Title",
-    question: "Question?",
-    choices: ["Option3", "Option2", "Option1", "Option1"],
-  };
-  return res.render("results", templateVars);
+  // get results from database
+  const pollsPromise = getPollById(id);
+  const choicesPromise = getChoicesByPoll(id);
+  const resultsPromise = getResultsById(id).then(generateResults);
+  Promise.all([pollsPromise, choicesPromise, resultsPromise]).then(
+    ([poll, choices, results]) => {
+      const templateVars = {
+        title: poll.title,
+        question: poll.question,
+        choices: rankChoices(choices, results).map((choice) => choice.title),
+      };
+      return res.render("results", templateVars);
+    }
+  );
 });
 
 module.exports = router;
