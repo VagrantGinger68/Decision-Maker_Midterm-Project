a39de0b147a86f61c26b45d016ec50227a34fc6e
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-09-27 10:36:47 -0400

    Fixed ESLint errors

--- routes/poll.js
+++ routes/poll.js
@@ -21,30 +21,30 @@ router.post("/", (req, res) => {
   const choices = getChoices(req.body);
 
   userQueries.createUser(email)
-  .then(user=>{
-    return pollQueries.insertPoll([pollId, pollTitle, questionTitle, user.id]);
-  })
-  .then(() => {
-    return insertAllChoices(choices, pollId);
-  })
-  .then (() => {
-    const emailInfo = {
-      from: "Decision Maker <postmaster@sandboxc01919f6d65b40f7bba14ed0dc9d97bd.mailgun.org>",
-      to: email,
-      subject: "Here's your Poll!",
-      html: `<div style='height: 100vh; background-color: #fcf4de; display: flex; flex-direction: column; align-items:center;'><img style='height: 400px' src='https://github.com/VagrantGinger68/Lighthouse-Midterm/blob/ryan/public/images/decision-maker-logo.png?raw=true'>
-      <h1>Here are the links for your poll!</h1>
-      <h2>Poll Results: <a href="http://localhost:8080/results/${pollId}">http:localhost:8080/results/${pollId}</a></h2>
-      <h2>Share Poll: <a href="http://localhost:8080/submissions/${pollId}">http:localhost:8080/submissions/${pollId}</a></h2>
-      </div>
-      `
-    };
-    return mailgun().messages().send(emailInfo);
-  })
-  .then (() => {
-    const templateVars = {email};
-    res.render("poll_submitted", templateVars);
-  })
+    .then(user=>{
+      return pollQueries.insertPoll([pollId, pollTitle, questionTitle, user.id]);
+    })
+    .then(() => {
+      return insertAllChoices(choices, pollId);
+    })
+    .then(() => {
+      const emailInfo = {
+        from: "Decision Maker <postmaster@sandboxc01919f6d65b40f7bba14ed0dc9d97bd.mailgun.org>",
+        to: email,
+        subject: "Here's your Poll!",
+        html: `<div style='height: 100vh; background-color: #fcf4de; display: flex; flex-direction: column; align-items:center;'><img style='height: 400px' src='https://github.com/VagrantGinger68/Lighthouse-Midterm/blob/ryan/public/images/decision-maker-logo.png?raw=true'>
+        <h1>Here are the links for your poll!</h1>
+        <h2>Poll Results: <a href="http://localhost:8080/results/${pollId}">http:localhost:8080/results/${pollId}</a></h2>
+        <h2>Share Poll: <a href="http://localhost:8080/submissions/${pollId}">http:localhost:8080/submissions/${pollId}</a></h2>
+        </div>
+        `
+      };
+      return mailgun().messages().send(emailInfo);
+    })
+    .then(() => {
+      const templateVars = {email};
+      res.render("poll_submitted", templateVars);
+    });
 });
 
 module.exports = router;
